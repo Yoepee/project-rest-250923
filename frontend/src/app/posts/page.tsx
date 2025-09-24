48d2a4a248ab028c51ba38b0833a7a0e29d539a2
Yoepee <kdy2975@naver.com>
2025-09-24 14:09:57 +0900

    prettier 세팅

--- frontend/src/app/posts/page.tsx
+++ frontend/src/app/posts/page.tsx
@@ -1,44 +1,43 @@
-'use client'
+"use client";
 
-import { components } from '@/lib/backend/apiV1/schema'
-import { apiFetch } from '@/lib/backend/client'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import { components } from "@/lib/backend/apiV1/schema";
+import { apiFetch } from "@/lib/backend/client";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Page() {
-    type PostDto = components['schemas']['PostDto']
-    const [posts, setPosts] = useState<PostDto[] | null>(null)
+  type PostDto = components["schemas"]["PostDto"];
+  const [posts, setPosts] = useState<PostDto[] | null>(null);
 
-    useEffect(() => {
-        // (data) => setPosts(data) == setPosts
-        apiFetch('/api/v1/posts')
-            .then(setPosts)
-            .catch((error) => {
-                alert(`${error.resultCode}: ${error.msg}`)
-            })
-    }, [])
-    if (posts === null) return <div>로딩중...</div>
+  useEffect(() => {
+    // (data) => setPosts(data) == setPosts
+    apiFetch("/api/v1/posts")
+      .then(setPosts)
+      .catch((error) => {
+        alert(`${error.resultCode}: ${error.msg}`);
+      });
+  }, []);
+  if (posts === null) return <div>로딩중...</div>;
 
-    return (
-        <>
-            <h1>글 목록</h1>
-            {posts.length === 0 && <div>글이 없습니다.</div>}
+  return (
+    <>
+      <h1>글 목록</h1>
+      {posts.length === 0 && <div>글이 없습니다.</div>}
 
-            {posts.length > 0 && (
-                <ul>
-                    {posts.map((post) => (
-                        <li key={post.id}>
-                            {post.id} /
-                            <Link href={`/posts/${post.id}`}>{post.title}</Link>{' '}
-                            /{post.content}
-                        </li>
-                    ))}
-                </ul>
-            )}
+      {posts.length > 0 && (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              {post.id} /<Link href={`/posts/${post.id}`}>{post.title}</Link> /
+              {post.content}
+            </li>
+          ))}
+        </ul>
+      )}
 
-            <div>
-                <Link href="/posts/write">글쓰기</Link>
-            </div>
-        </>
-    )
+      <div>
+        <Link href="/posts/write">글쓰기</Link>
+      </div>
+    </>
+  );
 }
