26a8b9919c624f4de4eedce8d669857ad535e5b9
Yoepee <kdy2975@naver.com>
2025-09-25 10:48:56 +0900

    로그인 헤더에 경로 추가

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -38,6 +38,12 @@ export default function RootLayout({
             <Link href="/posts" className="p-2 rounded hover:bg-gray-100">
               글목록
             </Link>
+            <Link
+              href="/members/login"
+              className="p-2 rounded hover:bg-gray-100"
+            >
+              로그인
+            </Link>
           </nav>
         </header>
         <main className="flex-1 flex flex-col">{children}</main>
