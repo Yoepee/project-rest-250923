079e0091d6065d21c7b0ed3f9e51e84af5c98296
Yoepee <kdy2975@naver.com>
2025-09-25 14:37:04 +0900

    레이아웃 컴포넌트 분리

---
+++ frontend/src/app/ClientLayout.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+const ClientLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <header>
+        <nav className="flex">
+          <Link href="/" className="p-2 rounded hover:bg-gray-100">
+            홈
+          </Link>
+          <Link href="/posts" className="p-2 rounded hover:bg-gray-100">
+            글목록
+          </Link>
+          <Link href="/members/login" className="p-2 rounded hover:bg-gray-100">
+            로그인
+          </Link>
+        </nav>
+      </header>
+      <main className="flex-1 flex flex-col">{children}</main>
+      <footer className="text-center p-2">풋터</footer>
+    </>
+  );
+};
+
+export default ClientLayout;
