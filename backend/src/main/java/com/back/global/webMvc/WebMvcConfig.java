b6b42dfbf9802d0c1eb3dc75def92c3b48c480ed
Yoepee <kdy2975@naver.com>
2025-09-29 15:48:34 +0900

    SecurityConfig에서 처리하는 내용 중복 제거

--- backend/src/main/java/com/back/global/webMvc/WebMvcConfig.java
+++ backend/src/main/java/com/back/global/webMvc/WebMvcConfig.java
@@ -1,18 +1,15 @@
 package com.back.global.webMvc;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/api/v1/**")
-                .allowedOrigins("https://cdpn.io", "http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-}
+// SecurityConfig에서 처리를 해줘서 필요 없어짐.
+//@Configuration
+//public class WebMvcConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry
+//                .addMapping("/api/v1/**")
+//                .allowedOrigins("https://cdpn.io", "http://localhost:3000")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
+//}
